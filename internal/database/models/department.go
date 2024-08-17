401cd9af81dc4d59d487ce4537ab401e92f33b3c
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-08-17 10:02:33 +0330

    feat(sahma): department model

--- internal/database/models/department.go
+++ internal/database/models/department.go
@@ -1,4 +1,13 @@
 package models
 
+import "time"
+
 type Department struct {
+	ID              uint
+	Name            string
+	CreatedBy       uint
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+	DepartmentUsers []*DepartmentUser
+	Folders         []*Folder
 }
