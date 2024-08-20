c75af511b34924f0b3f7ba2e6dacb445d806f34b
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-08-20 10:45:26 +0330

    feat(sahma): letter inbox model

--- internal/database/models/letter_inbox.go
+++ internal/database/models/letter_inbox.go
@@ -1,4 +1,20 @@
 package models
 
+import "time"
+
 type LetterInbox struct {
+	ID               uint
+	LetterID         *uint
+	UserID           uint
+	ReadStatus       string
+	IsRefer          *string
+	ReferredBy       uint
+	ReferDescription *string
+	DueDate          *string
+	Meta             *string
+	CreatedAt        time.Time
+	UpdatedAt        *time.Time
+	Letter           *Letter
+	ReferrerUser     *User
+	User             *User
 }
