83fc960c1a71e950e232511bb074cb657b3e7e1c
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-08-20 12:08:11 +0330

    feat(sahma): role model

--- internal/database/models/role.go
+++ internal/database/models/role.go
@@ -1,4 +1,13 @@
 package models
 
+import "time"
+
 type Role struct {
+	ID           uint
+	Title        string
+	Slug         string
+	PermissionID uint
+	CreatedAt    time.Time
+	UpdatedAt    *time.Time
+	Permission   *Permission
 }
