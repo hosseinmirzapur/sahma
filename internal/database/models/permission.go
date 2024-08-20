3b9689be7511b98533136950c561bfa899111caf
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-08-20 12:04:16 +0330

    feat(sahma): permission model

--- internal/database/models/permission.go
+++ internal/database/models/permission.go
@@ -1,4 +1,14 @@
 package models
 
+import "time"
+
 type Permission struct {
+	ID        uint
+	Full      int
+	Modify    int
+	ReadOnly  int
+	CreatedAt time.Time
+	UpdatedAt *time.Time
+	RoleID    uint
+	Role      Role
 }
