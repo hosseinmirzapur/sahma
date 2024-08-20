7ac11c18270b03b0f391b93e7085a46b5d812a54
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-08-20 10:40:18 +0330

    feat(sahma): letter attachment model

--- internal/database/models/letter_attachment.go
+++ internal/database/models/letter_attachment.go
@@ -1,4 +1,15 @@
 package models
 
+import "time"
+
 type LetterAttachment struct {
+	ID             uint
+	Type           string
+	FileLocation   string
+	AttachableType string
+	AttachableID   uint
+	Meta           *string
+	CreatedAt      time.Time
+	UpdatedAt      *time.Time
+	Letter         *Letter
 }
