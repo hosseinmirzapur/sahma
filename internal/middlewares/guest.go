1556c74187ca23ade284c200994e74201d183fbd
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-08-24 00:01:08 +0330

    feat(sahma): workflow + server files

---
+++ internal/middlewares/guest.go
@@ -0,0 +1,9 @@
+package middlewares
+
+import "github.com/gin-gonic/gin"
+
+func Guest(guards ...string) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+	}
+}
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -3,5 +3,10 @@ package server
 import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(r *gin.Engine) {
+	// register user web routes
+	userWebRoutes(r)
+}
+
+func userWebRoutes(r *gin.Engine) {
 
 }
