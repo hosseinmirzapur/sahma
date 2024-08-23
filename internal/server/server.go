55a20b287650af47428e51d6e5258374f074137b
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-08-23 23:41:19 +0330

    feat(sahma): security headers & .env.example

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,13 +1,36 @@
 package server
 
 import (
+	"fmt"
+	"net/http"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func NewServer() *gin.Engine {
-	// create a default gin server
-	server := gin.Default()
+	// create gin server with configurations
+	server := gin.New()
+	server.Use(gin.Recovery(), func(c *gin.Context) {
+		if c.Request.Host != os.Getenv(fmt.Sprintf("%s:%s", os.Getenv("APP_HOST"), os.Getenv("APP_PORT"))) {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Invalid host header"})
+			return
+		}
+		c.Header("X-Frame-Options", "DENY")
+		c.Header(
+			"Content-Security-Policy",
+			"default-src 'self'; connect-src *; font-src *; script-src-elem * 'unsafe-inline'; img-src * data:; style-src * 'unsafe-inline';",
+		)
+		c.Header("X-XSS-Protection", "1; mode=block")
+		c.Header("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
+		c.Header("Referrer-Policy", "strict-origin")
+		c.Header("X-Content-Type-Options", "nosniff")
+		c.Header("Permissions-Policy",
+			"geolocation=(),midi=(),sync-xhr=(),microphone=(),camera=(),magnetometer=(),gyroscope=(),fullscreen=(self),payment=()",
+		)
+		c.Next()
+	})
 
 	// register routes on the server instance
 	RegisterRoutes(server)
